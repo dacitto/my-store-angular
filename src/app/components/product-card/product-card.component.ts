aaa0c5c660013a3ed02e9df922e9cb4282d33102
Salah daci <dacidigital@gmail.com>
2023-02-22 23:12:35 +0100

    feat: inject cart  service to product-card  component

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { CartItem } from './../../models/cart.d';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from 'src/app/models/product';
-
+import { CartService } from 'src/app/services/cart.service';
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
@@ -8,6 +9,20 @@ import { Product } from 'src/app/models/product';
 })
 export class ProductCardComponent {
   @Input() product!: Product;
-  constructor() {}
+  @Output() addToCart: EventEmitter<Product> = new EventEmitter();
+  constructor(private cartService: CartService) {}
+  amount = 1;
+
+  updateState(product: Product) {
+    if (confirm(`Are you sure you want to add ${product.name} to cart ?`)) {
+      const total = this.amount * product.price;
+      const newPoduct: CartItem = { ...product, total, amount: this.amount };
+      this.cartService.addToCart(newPoduct);
+    }
+  }
+
+  onInputChange(amount: number) {
+    this.amount = amount;
+  }
   ngOnInit(): void {}
 }
--- src/app/pages/cart-page/cart-page.component.ts
+++ src/app/pages/cart-page/cart-page.component.ts
@@ -1,10 +1,19 @@
+import { CartService } from 'src/app/services/cart.service';
 import { Component } from '@angular/core';
+import { CartList } from 'src/app/models/cart';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
-  styleUrls: ['./cart-page.component.scss']
+  styleUrls: ['./cart-page.component.scss'],
 })
 export class CartPageComponent {
+  products: CartList = [];
+  constructor(private cartService: CartService) {}
 
+  ngOnInit(): void {
+    this.products = this.cartService.getCart();
+    // Access the global state array in the service
+    console.log(this.products);
+  }
 }
