492c4586a7fa12733d455d489adcd697a90a6d59
Salah daci <dacidigital@gmail.com>
2023-02-25 15:40:39 +0100

    feat: inhance style in cart page

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -5,7 +5,17 @@ import { CartItem, CartList } from '../models/cart';
   providedIn: 'root',
 })
 export class CartService {
-  CartList: CartList = [];
+  CartList: CartList = [
+    {
+      amount: 1,
+      description: 'test',
+      id: 123,
+      name: 'test product',
+      price: 234,
+      total: 234,
+      url: 'https://images.unsplash.com/photo-1509391618207-32f1fa13c1d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8b3JhbmdlJTIwZnJ1aXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+    },
+  ];
   constructor() {
     console.log('constructor called and value of Cartlist is', this.CartList);
   }
